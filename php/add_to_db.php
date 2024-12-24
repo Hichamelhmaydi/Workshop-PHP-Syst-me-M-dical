dfb7ff972b45525b62f732050eaded13fac11d3d
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-24 13:01:50 +0100

    ajouter la requete pour insert data dans le tableaux pateint

--- php/add_to_db.php
+++ php/add_to_db.php
@@ -1,8 +1,21 @@
 <?php
 include("connc.php");
-$nom=$_POST['nom'];
-$prenom=$_POST['prenom'];
 
-$stmt=$$mysqli->prepare('');
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+
+
+$stmt = $mysqli->prepare("INSERT INTO patient (nom, prenom) VALUES (?, ?)");
+$stmt->bind_param("ss", $nom, $prenom); 
+if ($stmt->execute()) {
+    echo "Données insérées avec succès.";
+} else {
+    echo "Erreur lors de l'insertion : " . $stmt->error;
+}
+if (file_exists('../choix_dr.html')) {
+    header("Location: ../choix_dr.html");
+    exit;
+} 
+$stmt->close();
 
 ?>
